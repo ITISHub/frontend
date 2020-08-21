f0bc168762fdc3a3001f574c178baed9d9fec055
Daniil Trofimov <trofimovdanil946@gmail.com>
2020-08-21 23:27:22 +0300

    add docs directory

--- itis-hub/nuxt.config.js
+++ itis-hub/nuxt.config.js
@@ -20,7 +20,11 @@ export default {
   ** Nuxt target
   ** See https://nuxtjs.org/api/configuration-target
   */
-  target: 'server',
+  // target: 'server',
+  target: 'static',
+  router: {
+    base: '/frontend/'
+  },
   /*
   ** Headers of the page
   ** See https://nuxtjs.org/api/configuration-head
